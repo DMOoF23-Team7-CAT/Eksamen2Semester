2493cdbb9e85c9d20faf7d4df3e0b3bd5fbab334
jacsp <jspr54132@edu.ucl.dk>
2023-11-20 10:32:52 +0100

    Added CustomerRepository
    Doesn't implement UpdateCustomer yet

---
+++ KlatreKongen/KlatreKongen/Model/Repositories/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlatreKongen.Model.Repositories
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomerById(int customerId);
+        void InsertCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(int customerId);
+    }
+}
